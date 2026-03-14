530adc8a14f9f4cd6bd7f67907dbc139cdcdcaf0
Chakradhar-Malage <chessp526@gmail.com>
2026-03-14 21:01:05 +0530

    Initialized backend in spring boot

---
+++ backend/reint_wind/src/main/java/com/Chakradhar/reint_wind/ReintWindApplication.java
@@ -0,0 +1,13 @@
+package com.Chakradhar.reint_wind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReintWindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReintWindApplication.class, args);
+	}
+
+}
